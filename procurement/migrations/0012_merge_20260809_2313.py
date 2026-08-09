9876e821a774ac09cff0ecc2cb8808d7601dc19c
sidharth9472 <sidharths439@gmail.com>
2026-08-09 23:33:24 +0530

    Fix procurement migration history

---
+++ procurement/migrations/0012_merge_20260809_2313.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('procurement', '0008_remove_conveningorder_additional_members_and_more'),
+        ('procurement', '0010_eas_major_eas_minor_alter_notingsheet_branch_and_more'),
+    ]
+
+    operations = []
